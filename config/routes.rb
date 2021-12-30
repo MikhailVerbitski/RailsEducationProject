51580c1b032b75a7efe70f81d394d095ccdae258
Mikhail Viarbitsky <mikhail@MacBook-Pro-Mikhail.local>
2021-12-30 15:28:47 +0300

    created navbar and configure style env

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/update'
+  get 'posts/edit'
+  get 'posts/destroy'
+  get 'posts/show'
+  get 'posts', to: 'posts#index'
 end
