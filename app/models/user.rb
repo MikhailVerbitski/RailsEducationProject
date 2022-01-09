6842ba19f7e599137653f6bad5d417e45fa78e5f
Mikhail Viarbitsky <mikhail@MacBook-Pro-Mikhail.local>
2022-01-09 20:59:48 +0300

    fixed account confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable,
          :rememberable,
          :trackable,
-         :timeoutable
-         :validatable
+         :timeoutable,
+         :validatable,
+         :confirmable
 end
