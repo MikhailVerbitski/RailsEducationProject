c1cca827aee94f5b44d4d9f5153b4d13981db59e
MikhailVerbitski <misha.verbi@gmail.com>
2022-01-30 00:27:51 +0300

    created post list view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     options = %i[hard_wrap autolink no_intra_emphasis fenced_code_blocks]
     Markdown.new(text, *options).to_html.html_safe
   end
+
+  def markdown_text_only(text)
+    html = markdown(text)
+    Nokogiri::HTML.parse(html).text
+  end
 end
